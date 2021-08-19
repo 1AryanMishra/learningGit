9817d2bb74f0676fe5747402620a84501c1f6010
Aryan Mishra <mishra321123@gmail.com>
2021-08-19 18:25:37 +0530

    form added

---
+++ js/form/form.js
@@ -0,0 +1,13 @@
+var data = document;
+/*var naam = data.getElementById("name");
+var age = data.getElementById("age");
+var state = data.getElementById("state");
+var district = data.getElementById("district");
+var btn = data.getElementById("get_location");
+var location = data.getElementById("location");*/
+
+function fetch_location(){
+    console.log("Pta ni kya yaar!");
+}
+
+btn.addEventListener("click", fetch_location());
